72560f85d8c9cc0fe8f8ffa509100000ede51803
Peijin Zhang <peijin@ustc.edu>
2025-09-05 16:11:10 +0000

    minor

--- runSlurm_daily_refraCorr.sh
+++ runSlurm_daily_refraCorr.sh
@@ -11,8 +11,8 @@
 #SBATCH --mail-user=pz47@njit.edu
 
 
-DIRSOFT=/opt/devel/solarpipe/envs/lwasolarpipe/
-DIR_PY_ENV=/opt/devel/bin.chen/envs/suncasa/
+DIRSOFT=/opt/devel/solarpipe/operation/ovro-lwa-solar-ops/
+DIR_PY_ENV=/opt/devel/solarpipe/envs/lwasolarpipe/
 source /home/solarpipe/.bashrc
 conda activate $DIR_PY_ENV
 
